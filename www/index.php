6b9f0b20ddac055a9a963ae5f262b544a2f8dc70
guilhermehd903 <43217991+guilhermehd903@users.noreply.github.com>
2022-11-12 18:55:51 -0300

    print r

--- www/index.php
+++ www/index.php
@@ -1,12 +1,13 @@
 <?php
-header("Content-Type: application/json");
-header("Access-Control-Allow-Origin: *");
-$con = new PDO("mysql:dbname=meudb;host=db", "gui", "123"); 
-$alunos = $con->query("SELECT * FROM aluno");
-$data = $alunos->fetchAll();
-if($data != false){
-    $json = json_encode($data, JSON_FORCE_OBJECT);
-}
+// header("Content-Type: application/json");
+// header("Access-Control-Allow-Origin: *");
+// $con = new PDO("mysql:dbname=meudb;host=db", "gui", "123"); 
+// $alunos = $con->query("SELECT * FROM aluno");
+// $data = $alunos->fetchAll();
+// if($data != false){
+//     $json = json_encode($data, JSON_FORCE_OBJECT);
+// }
 
-echo $json;
+// echo $json;
+print_r($_SERVER);
 ?>
