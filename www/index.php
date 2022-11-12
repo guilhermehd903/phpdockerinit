6c6d9733271f4770e9ae3adfa508f79b0426210f
guilhermehd903 <43217991+guilhermehd903@users.noreply.github.com>
2022-11-12 18:26:19 -0300

    init project

---
+++ www/index.php
@@ -0,0 +1,12 @@
+<?php
+header("Content-Type: application/json");
+header("Access-Control-Allow-Origin: *");
+$con = new PDO("mysql:dbname=meudb;host=db", "gui", "123"); 
+$alunos = $con->query("SELECT * FROM aluno");
+$data = $alunos->fetchAll();
+if($data != false){
+    $json = json_encode($data, JSON_FORCE_OBJECT);
+}
+
+echo $json;
+?>
